4ba88b7ba8d5c23b82b3367f35bd8af835c8543e
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-23 20:54:50 +0100

    Added 2nd solution

--- week-4/W4-S1-JavaScript/3_Functions/exercise/script.js
+++ week-4/W4-S1-JavaScript/3_Functions/exercise/script.js
@@ -1,1 +1,11 @@
 // TODO: Add the code here
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+// or give a default number to b:
+// function multiplyNumers(a, b=2){
+//     return a * b;}
+
+let result = multiplyNumbers(5, 2);
+console.log("The result is: " + result);
