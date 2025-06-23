85334b6f545286f17668ad738ce015827820e4af
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-23 19:25:46 +0100

    Done TODOs

--- week-4/W4-S1-JavaScript/1_Variables/exercise/script.js
+++ week-4/W4-S1-JavaScript/1_Variables/exercise/script.js
@@ -1,8 +1,8 @@
 var firstName = "John";
 var surName = "Smith";
 
-const surName = "Doe";
-surName = "Doe";
+// const surName = "Doe";
+// surName = "Doe";
 
 var age = 25;
 var retirementAge = 67;
@@ -13,8 +13,13 @@ console.log(`or use template literals to output details for Mr. ${surName} `);
 
 // TODO: log out the full name "John Smith" by concatenating the variables firstName and surName
 
+console.log(firstName + " " + surName);
+
 // TODO: log out "John Smith and I amd 25 years old"
+console.log(`${firstName} ${surName} and I am ${age} years old`);
 
 // TODO: Create a variable to store the number of years until retirement
+var yearsLeft = retirementAge - age;
 
 // TODO: log out "I have 42 years until retirement"
+console.log(`I have ${yearsLeft} years until retirement`);
