a1d2d3afc938d5cfca4b25d13e47ed1b52ac1a4f
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-24 19:14:06 +0100

    Done W4-S2 Exercise 5

--- week-4/W4-S2-ObjectsLoops/5_ObjectsArrays/exercise/script.js
+++ week-4/W4-S2-ObjectsLoops/5_ObjectsArrays/exercise/script.js
@@ -1,3 +1,30 @@
 // create your coffee object array here
+var orders = [
+  { type: "latte", milk: true, customer: "Alice" },
+  { type: "espresso", milk: false, customer: "Bob" },
+  { type: "cappuccino", milk: true, customer: "Charlie" },
+];
+// create your print order function here
+function printOrders(orderList) {
+  for (var i = 0; i < orderList.length; i++) {
+    var order = orderList[i];
+    var milk;
 
-// creat your print order function here
+    if (order.milk === true) {
+      milk = "Yes";
+    } else {
+      milk = "No";
+    }
+
+    console.log(
+      "Customer: " +
+        order.customer +
+        ", Coffee: " +
+        order.type +
+        ", Milk: " +
+        milk
+    );
+  }
+}
+
+printOrders(orders);
