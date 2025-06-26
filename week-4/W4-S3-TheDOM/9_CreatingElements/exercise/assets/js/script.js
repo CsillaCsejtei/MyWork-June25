48ad81245ff5f9ef99eacf03ce4503037db3778c
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-26 21:17:52 +0100

    Done W4-S3 Exercise 9.

--- week-4/W4-S3-TheDOM/9_CreatingElements/exercise/assets/js/script.js
+++ week-4/W4-S3-TheDOM/9_CreatingElements/exercise/assets/js/script.js
@@ -1,3 +1,25 @@
 // TODO: Add 2 new items to the sidebar called "Register" and "Help".
+var sideBarEl=document.querySelector(".sidebar ul");
+
+var registerEl = document.createElement("li");
+
+var helpEl = document.createElement("li");
+registerEl.textContent = "Register";
+helpEl.textContent = "Help";
+sideBarEl.appendChild(registerEl);
+sideBarEl.appendChild(helpEl);
+
 
 // TODO: MEGA CHALLENGE: can you add the Help link between Reports and Settings?
+
+var sideBarEl = document.querySelector(".sidebar ul");
+
+var registerEl = document.createElement("li");
+registerEl.textContent = "Register";
+sideBarEl.appendChild(registerEl);
+
+var helpEl = document.createElement("li");
+helpEl.textContent = "Help";
+
+// Insert Help before the 4th item (Settings)
+sideBarEl.insertBefore(helpEl, sideBarEl.children[3]);
