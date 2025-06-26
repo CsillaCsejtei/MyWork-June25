d2945ebdaf34ec53fc9afd19a308eef71a77b22a
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-26 21:32:35 +0100

    Added Week 5

--- week-4/W4-S3-TheDOM/10_EventHandling/exercise/assets/js/script.js
+++ week-4/W4-S3-TheDOM/10_EventHandling/exercise/assets/js/script.js
@@ -10,12 +10,17 @@ searchButton.addEventListener("click", onClickSearhButton);
 function onClickSearhButton() {
   // 4. use the value property of the searchInput to get the search term
   //TODO:
+  var term = searchTerm.value;
   // 5. select the searches div using document.getElementById
   //TODO:
+  var searchesEl = document.getElementById("searches");
   // 6. create a new li element using document.createElement
   //TODO:
+  var newLiEl = document.createElement("li");
   // 7. set the innerHTML of the new paragraph to the search term
   //TODO:
+  newLiEl.textContent = term;
   // 8. append the new paragraph to the searches div
   //TODO:
+  searchesEl.appendChild(newLiEl);
 }
