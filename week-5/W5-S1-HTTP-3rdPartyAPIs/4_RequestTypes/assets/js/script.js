7a57984f45dba400efcdbf0fa242bac5be071738
CsillaCsejtei <csillacsejtei@gmail.com>
2025-06-30 20:42:21 +0100

    Done W5-S1 4. exercise

--- week-5/W5-S1-HTTP-3rdPartyAPIs/4_RequestTypes/assets/js/script.js
+++ week-5/W5-S1-HTTP-3rdPartyAPIs/4_RequestTypes/assets/js/script.js
@@ -42,3 +42,8 @@ document.getElementById("fetch").addEventListener("click", sendFetchRequest);
 document.getElementById("axios").addEventListener("click", sendAxiosRequest);
 document.getElementById("ajax").addEventListener("click", sendAjaXRequest);
 document.getElementById("xhr").addEventListener("click", sendXHRRequest);
+
+//Easiest to use: Axios, because it has simple syntax and automatic JSON parsing.
+//Best error handling: Axios, since it automatically throws errors on HTTP failures with detailed info.
+//What to use for a modern web app: Fetch API or Axios, depending on project needs and preferences.
+//Where legacy methods are useful: In maintaining old jQuery projects or very old browser support requiring XHR.
