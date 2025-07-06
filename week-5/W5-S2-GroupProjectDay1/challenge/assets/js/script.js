19a4bd2eae916e20a2e00dd0f9c5fd0780df5970
CsillaCsejtei <csillacsejtei@gmail.com>
2025-07-06 21:45:05 +0100

    Added Week 6

--- week-5/W5-S2-GroupProjectDay1/challenge/assets/js/script.js
+++ week-5/W5-S2-GroupProjectDay1/challenge/assets/js/script.js
@@ -1,7 +1,24 @@
-const API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
-
-// First, get the latitude and longitude for the city
-const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
-
-// Call getWeather API when the button is clicked
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+function search(city) {
+  const API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
+  // First, get the latitude and longitude for the city
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+  axios
+    .get(geoUrl)
+    .then(function (response) {
+      const lat = response.data[0].lat;
+      const lon = response.data[0].lon;
+      // Call getWeather API when the button is clicked
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+      return axios.get(weatherUrl);
+    })
+    .then(function (response) {
+      const temp = response.data.main.temp;
+      document.getElementById(
+        "weatherResult"
+      ).innerHTML = `Temperature: ${temp} °C`;
+    });
+}
+document.getElementById("getWeather").addEventListener("click", function () {
+  const city = document.getElementById("city").value;
+  search(city);
+});
